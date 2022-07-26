1cffabe652e79f7f72a67116586c2bf6a5c4869e
Daniel Españadero <developer@daniel-espanadero.com>
2022-07-26 16:08:14 +0200

    update

--- src/model/recomendation.model.js
+++ src/model/recomendation.model.js
@@ -4,27 +4,27 @@ const RecomendationsSchema = Schema(
     {
         reco_id: {
             type: String,
-            require: [true, 'Reco id is required']
+            required: [true, 'Reco id is required']
         },
         time_reco: {
             type: Date,
-            require: [true, 'time reco is required']
+            required: [true, 'time reco is required']
         },
         id_sender: {
             type: String,
-            require: [true, 'id sender is required']
+            required: [true, 'id sender is required']
         },
         id_reciever: {
             type: String,
-            require: [true, 'id reciever is required']
+            required: [true, 'id reciever is required']
         },
         id_product: {
             type: String,
-            require: [true, 'id product is required']
+            required: [true, 'id product is required']
         },
         product_bought: {
             type: Number,
-            require: [true, 'Product bought is required']
+            required: [true, 'Product bought is required']
         },
     },
     {
